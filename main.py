58bb18fde3e0d7dc05e936aa91683dc1d3270ca1
dukendev <ysanjeet535@gmail.com>
2023-12-08 03:54:40 +0530

    solved day 6

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+def concatenate_strings_to_integer(str_list):
+    concatenated_str = "".join(str_list)
+    result = int(concatenated_str)
+    return result
+
+
 test_path = "./input.txt"
 
 with open(test_path, "r") as file:
@@ -6,14 +12,14 @@ with open(test_path, "r") as file:
 
 time_raw = time_content.split(":")[1].strip().split(" ")
 time = list(filter(lambda x: x != "", time_raw))
+time_of_race = concatenate_strings_to_integer(time)
 time_nums = list(map(int, time))
-print(time_nums)
 
 
 distance_raw = distance_content.split(":")[1].strip().split(" ")
 distance = list(filter(lambda x: x != "", distance_raw))
+distance_of_race = concatenate_strings_to_integer(distance)
 distance_nums = list(map(int, distance))
-print(distance_nums)
 
 
 product = 1
@@ -26,3 +32,11 @@ for i, t in enumerate(time_nums):
 
 
 print(product)
+
+
+c = 0
+for j in range(0, time_of_race):
+    if distance_of_race < (j * (time_of_race - j)):
+        c += 1
+
+print(c)
